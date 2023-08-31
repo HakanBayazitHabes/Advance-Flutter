a0d98a4af9ea3073bbe7d7a959e3b46516ae23e3
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-31 18:12:00 +0300

    Adding Text Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -7,52 +7,62 @@ import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    //main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-    hintColor: ColorManager.grey,
-    splashColor: ColorManager.primaryOpacity70,
-
-    //card view theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      elevation: AppSize.s4,
-      shadowColor: ColorManager.grey,
-    ),
-
-    //app bar theme
-    appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.primaryOpacity70,
-        titleTextStyle: getRegularTextStyle(
-            color: ColorManager.white, fontSize: FontSize.s12)),
-
-    //button theme
-
-    buttonTheme: ButtonThemeData(
-      shape: StadiumBorder(),
+      //main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
-      buttonColor: ColorManager.primary,
+      hintColor: ColorManager.grey,
       splashColor: ColorManager.primaryOpacity70,
-    ),
 
-    //elevated Button theme
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        textStyle: getRegularTextStyle(color: ColorManager.white),
-        primary: ColorManager.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(AppSize.s8),
+      //card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.grey,
+      ),
+
+      //app bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularTextStyle(
+              color: ColorManager.white, fontSize: FontSize.s12)),
+
+      //button theme
+
+      buttonTheme: ButtonThemeData(
+        shape: StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70,
+      ),
+
+      //elevated Button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularTextStyle(color: ColorManager.white),
+          primary: ColorManager.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.s8),
+          ),
         ),
       ),
-    ),
 
-    //text theme
+      //text theme
+      textTheme: TextTheme(
+        headline1: getSemiBoldTextStyle(
+            color: ColorManager.darkPrimary, fontSize: FontSize.s16),
+        subtitle1: getMediumTextStyle(
+            color: ColorManager.lightGrey, fontSize: FontSize.s14),
+        caption: getRegularTextStyle(color: ColorManager.grey),
+        bodyText1: getRegularTextStyle(color: ColorManager.grey1),
+      )
 
-    //input decoration theme (text form field)
-  );
+      //input decoration theme (text form field)
+
+
+      );
 }
