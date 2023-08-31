9df966ab44324b1f0a2de09ca67a8813f27f37cc
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-08-31 16:57:20 +0300

    Adding App Bar Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,30 +1,40 @@
 import 'package:advance_flutter/presentation/color_manager.dart';
+import 'package:advance_flutter/presentation/styles_manager.dart';
 import 'package:flutter/material.dart';
 
+import 'font_manager.dart';
 import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      //main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      hintColor: ColorManager.grey,
+    //main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    hintColor: ColorManager.grey,
+    splashColor: ColorManager.primaryOpacity70,
 
-      //card view theme
-      cardTheme: CardTheme(
-        color: ColorManager.white,
+    //card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.grey,
+    ),
+
+    //app bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
         elevation: AppSize.s4,
-        shadowColor: ColorManager.grey,
-      )
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularTextStyle(
+            color: ColorManager.white, fontSize: FontSize.s12)),
 
-      //app bar theme
+    //button theme
 
-      //button theme
+    //text theme
 
-      //text theme
-
-      //input decoration theme (text form field)
-      );
+    //input decoration theme (text form field)
+  );
 }
