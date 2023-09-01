6a488b4e0576c43338932106697ca0c8ad5eb21d
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-02 00:24:04 +0300

    Start Adding Routes Manager

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,5 +1,5 @@
-import 'package:advance_flutter/presentation/color_manager.dart';
-import 'package:advance_flutter/presentation/styles_manager.dart';
+import 'package:advance_flutter/presentation/resources/color_manager.dart';
+import 'package:advance_flutter/presentation/resources/styles_manager.dart';
 import 'package:flutter/material.dart';
 
 import 'font_manager.dart';
