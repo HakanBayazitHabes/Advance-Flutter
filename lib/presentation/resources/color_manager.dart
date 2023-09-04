5cac5cf2c6360ecc351037346c04c35fde89793a
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-04 12:13:40 +0300

    Start Splash Screen

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color primary = HexColor.fromHex("#ED9728");
-  static Color secondary = HexColor.fromHex("#525252");
+  static Color darkGrey = HexColor.fromHex("#525252");
   static Color grey = HexColor.fromHex("#737477");
   static Color lightGrey = HexColor.fromHex("#9E9E9E");
   static Color primaryOpacity70 = HexColor.fromHex("#B3ED9728");
@@ -16,10 +16,11 @@ class ColorManager {
 }
 
 extension HexColor on Color {
-  static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = 'FF' + hexColorString;
+    }
+    return Color(int.parse(hexColorString, radix: 16));
   }
 }
