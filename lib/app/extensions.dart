154fce07def7537af540fb5e30d643f60f1d69b7
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-07 15:57:32 +0300

    Application Layer - Creating Extensions Functions

---
+++ lib/app/extensions.dart
@@ -0,0 +1,21 @@
+// extensions on String
+extension NonNullString on String? {
+  String orEmpty() {
+    if (this == null) {
+      return "";
+    } else {
+      return this!;
+    }
+  }
+}
+
+// extensions on Integer
+extension NonNullInteger on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
