407e6fb72d7fc2aba7170cee30bc66d6e963036d
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-08 12:11:02 +0300

    Data Layer - Adding Responses Messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -14,7 +14,7 @@ enum DataSource {
   NO_INTERNET_CONNECTION,
 }
 
-class ResponseClass {
+class ResponseCode {
   //API status codes
   static const int SUCCESS = 200;
   static const int NO_CONTENT = 201;
@@ -32,4 +32,24 @@ class ResponseClass {
   static const int SEND_TIMEOUT = -5;
   static const int CACHE_ERROR = -6;
   static const int NO_INTERNET_CONNECTION = -7;
+}
+
+class ResponseMessage {
+  //API status codes
+  static const String SUCCESS = "Success";
+  static const String NO_CONTENT = "Success with no content";
+  static const String BAD_REQUEST = "Bad request, try again later";
+  static const String UNAUTHORIZED = "User is unauthorised, try again later";
+  static const String FORBIDDEN = "User is forbidden, try again later";
+  static const String NOT_FOUND = "Url is not found, try again later";
+  static const String INTERNAL_SERVER_ERROR = "Some thing went wrong, try again later";
+
+  //local status code
+  static const String UNKNOWN_ERROR = "Some thing went wrong, try again later";
+  static const String CONNECTION_TIMEOUT = "Timeout error, try again later";
+  static const String CANCEL = "Request was cancelled, try again later";
+  static const String RECEIVE_TIMEOUT = "Timeout error, try again later";
+  static const String SEND_TIMEOUT = "Timeout error, try again later";
+  static const String CACHE_ERROR = "Cache error, try again later";
+  static const String NO_INTERNET_CONNECTION = "Please check your internet connection";
 }
