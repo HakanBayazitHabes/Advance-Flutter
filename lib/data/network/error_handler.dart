756707f89dfff34320f852da856455da5a81c20f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-08 11:55:18 +0300

    Data Layer - Adding Responses Codes

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -13,3 +13,23 @@ enum DataSource {
   CACHE_ERROR,
   NO_INTERNET_CONNECTION,
 }
+
+class ResponseClass {
+  //API status codes
+  static const int SUCCESS = 200;
+  static const int NO_CONTENT = 201;
+  static const int BAD_REQUEST = 400;
+  static const int UNAUTHORIZED = 401;
+  static const int FORBIDDEN = 403;
+  static const int NOT_FOUND = 404;
+  static const int INTERNAL_SERVER_ERROR = 500;
+
+  //local status code
+  static const int UNKNOWN_ERROR = -1;
+  static const int CONNECTION_TIMEOUT = -2;
+  static const int CANCEL = -3;
+  static const int RECEIVE_TIMEOUT = -4;
+  static const int SEND_TIMEOUT = -5;
+  static const int CACHE_ERROR = -6;
+  static const int NO_INTERNET_CONNECTION = -7;
+}
