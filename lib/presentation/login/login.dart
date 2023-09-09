a3316605ad736f94c179e0cd2aeb95f3e4fcba18
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-09 15:57:24 +0300

    Login View UI

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,5 +1,10 @@
 import 'package:advance_flutter/presentation/login/login_viewmodel.dart';
+import 'package:advance_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../resources/assets_manager.dart';
+import '../resources/color_manager.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -13,6 +18,7 @@ class _LoginViewState extends State<LoginView> {
 
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _loginViewModel.start();
@@ -33,6 +39,40 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+        body: Container(
+      padding: EdgeInsets.only(top: AppPadding.p100),
+      color: ColorManager.white,
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SvgPicture.asset(ImageAssets.loginIc),
+              SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                  stream: _loginViewModel.outputIsUserNameValid,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _userNameController,
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 
   @override
