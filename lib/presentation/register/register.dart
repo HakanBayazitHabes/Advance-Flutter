9406409e32c3a8d8392466ec831151f4deb16fef
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-15 12:51:09 +0300

    Adding UI for Register Screen

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 
 import '../../app/di.dart';
 import '../common/state_renderer/state_render_impl.dart';
+import '../resources/assets_manager.dart';
+import '../resources/routes_manager.dart';
+import '../resources/strings_manager.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -23,7 +26,7 @@ class _RegisterViewState extends State<RegisterView> {
       TextEditingController();
   TextEditingController _userEmailTextEditingController =
       TextEditingController();
-  TextEditingController _userPasswordTextEditingController =
+  TextEditingController _passwordTextEditingController =
       TextEditingController();
 
   @override
@@ -43,8 +46,8 @@ class _RegisterViewState extends State<RegisterView> {
     _userEmailTextEditingController.addListener(() {
       _viewModel.setEmail(_userEmailTextEditingController.text);
     });
-    _userPasswordTextEditingController.addListener(() {
-      _viewModel.setPassword(_userPasswordTextEditingController.text);
+    _passwordTextEditingController.addListener(() {
+      _viewModel.setPassword(_passwordTextEditingController.text);
     });
   }
 
@@ -73,7 +76,120 @@ class _RegisterViewState extends State<RegisterView> {
   }
 
   Widget _getContentWidget() {
-    return Center();
+    return Container(
+      padding: EdgeInsets.only(top: AppPadding.p100),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage(ImageAssets.splashLogo),
+              ),
+              SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<String?>(
+                  stream: _viewModel.outputErrorUserName,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _userNameTextEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppStrings.userName,
+                        labelText: AppStrings.userName,
+                        errorText: snapshot.data,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<String?>(
+                  stream: _viewModel.outputErrorPassword,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: _passwordTextEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppStrings.password,
+                        labelText: AppStrings.password,
+                        errorText: snapshot.data,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: AppSize.s28,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: AppPadding.p28, right: AppPadding.p28),
+                child: StreamBuilder<bool>(
+                  stream: _viewModel.outputIsAllInputsValid,
+                  builder: (context, snapshot) {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: AppSize.s40,
+                      child: ElevatedButton(
+                        onPressed: (snapshot.data ?? false)
+                            ? () {
+                                _viewModel.register();
+                              }
+                            : null,
+                        child: Text(AppStrings.login),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: AppPadding.p8,
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        //Go to login screen
+                        Navigator.pushNamed(
+                            context, Routes.forgotPasswordRoute);
+                      },
+                      child: Text(
+                        AppStrings.forgotPassword,
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        //Go to login screen
+                        Navigator.pushNamed(context, Routes.registerRoute);
+                      },
+                      child: Text(
+                        AppStrings.registerText,
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
