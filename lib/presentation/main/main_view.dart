23e897ea00eabfe3cb7f28fccdaf7674fc1467f4
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-16 15:06:46 +0300

    Finalising Adding Pages inside Main View

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -1,6 +1,8 @@
 import 'package:advance_flutter/presentation/main/search_page.dart';
 import 'package:advance_flutter/presentation/main/settings_page.dart';
+import 'package:advance_flutter/presentation/resources/color_manager.dart';
 import 'package:advance_flutter/presentation/resources/strings_manager.dart';
+import 'package:advance_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
@@ -21,6 +23,13 @@ class _MainViewState extends State<MainView> {
     SettingPage(),
   ];
 
+  List<String> titles = [
+    AppStrings.home,
+    AppStrings.search,
+    AppStrings.notifications,
+    AppStrings.settings,
+  ];
+
   var _title = AppStrings.home;
   var _currentIndex = 0;
 
@@ -34,6 +43,35 @@ class _MainViewState extends State<MainView> {
         ),
       ),
       body: pages[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: ColorManager.black,
+              blurRadius: AppSize.s1_5,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: ColorManager.primary,
+          unselectedItemColor: ColorManager.grey,
+          currentIndex: _currentIndex,
+          onTap: onTap,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home)),
+            BottomNavigationBarItem(icon: Icon(Icons.search)),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications)),
+            BottomNavigationBarItem(icon: Icon(Icons.settings)),
+          ],
+        ),
+      ),
     );
+  }
+
+  onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+      _title = titles[index];
+    });
   }
 }
