b283c34730125c5b8c19e65c9668957d41e0752f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2023-09-17 18:58:55 +0300

    Adding Banner Carousel Widget

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:advance_flutter/domain/model/model.dart';
 import 'package:advance_flutter/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:advance_flutter/presentation/main/home/home_viewmodel.dart';
 import 'package:advance_flutter/presentation/resources/strings_manager.dart';
 import 'package:advance_flutter/presentation/resources/values_manager.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../../app/di.dart';
+import '../../resources/color_manager.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -31,15 +34,16 @@ class _HomePageState extends State<HomePage> {
     return Center(
       child: SingleChildScrollView(
           child: StreamBuilder<FlowState>(
-        stream: _viewModel.outputState,
-        builder: (context, snapshot) {
-          return snapshot.data?.getScreenWidget(context, _getContentWidgets(),
-                  () {
-                _viewModel.start();
-              }) ??
-              Container();
-        },
-      )),
+            stream: _viewModel.outputState,
+            builder: (context, snapshot) {
+              return snapshot.data?.getScreenWidget(
+                  context, _getContentWidgets(),
+                      () {
+                    _viewModel.start();
+                  }) ??
+                  Container();
+            },
+          )),
     );
   }
 
@@ -65,26 +69,68 @@ class _HomePageState extends State<HomePage> {
           bottom: AppPadding.p8),
       child: Text(
         title,
-        style: Theme.of(context).textTheme.headline3,
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline3,
       ),
     );
   }
 
   Widget _getBannersCarousel() {
-    return Center();
+    return StreamBuilder<List<BannerAd>>(
+        stream: _viewModel.outputBanners,
+        builder: (context, snapshot) {
+          return _getBanner(snapshot.data);
+        });
   }
 
-  Widget _getService() {
-    return Center();
-  }
+  Widget _getBanner(List<BannerAd>? banners) {
+    if (banners != null) {
+      return CarouselSlider(
+        items: banners
+            .map((banner) =>
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: AppSize.s1_5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(AppSize.s8),
+                  side: BorderSide(
+                      color: ColorManager.white, width: AppSize.s1_5),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(AppSize.s12),
+                  child: Image.network(
+                    banner.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ))
+            .toList(),
+        options: CarouselOptions(
+          height: AppSize.s190,
+          autoPlay: true,
+          enableInfiniteScroll: true,
+          enlargeCenterPage: true,
+        ),
+      );
+    } else {
+      return Container();
+    }
 
-  Widget _getStore() {
-    return Center();
-  }
+    Widget _getService() {
+      return Center();
+    }
 
-  @override
-  void dispose() {
-    _viewModel.dispose();
-    super.dispose();
+    Widget _getStore() {
+      return Center();
+    }
+
+    @override
+    void dispose() {
+      _viewModel.dispose();
+      super.dispose();
+    }
   }
-}
